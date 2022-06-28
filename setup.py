c93717397e7a731c16c3c7022bed927ab673c5be
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2022-06-28 10:50:13 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as readmefile:
+    readme = readmefile.read()
+
+setup(
+    name='localstorage',
+    version='1.0.0',
+    author='David Ferreira',
+    author_email='ferreirad08@gmail.com',
+    description='A package to store data on hard disk (HD) and make it available to all Python applications running in parallel!',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ferreirad08/localstorage',
+    packages=find_packages(),
+    install_requires=[],
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8.6',
+)
