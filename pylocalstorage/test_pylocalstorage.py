4d3b4576522eb5059d35ca42e729b2314bb4b1c0
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2022-07-28 12:56:40 -0400

    Create test_pylocalstorage.py

---
+++ pylocalstorage/test_pylocalstorage.py
@@ -0,0 +1,27 @@
+"""Tests for LocalStorage."""
+
+from pylocalstorage import LocalStorage
+import pytest
+
+
+class TestLocalStorage:
+
+    @pytest.mark.parametrize(
+        "key, value",
+        (
+            ("x", "this"),
+            ("x", "localhost"),
+        ),
+    )
+    def test_setItem(self, key, value):
+        storage = LocalStorage()
+        storage.setItem(key, value)
+        x = storage.getItem(key)
+        assert x == value
+
+    def test_removeItem(self):
+        storage = LocalStorage()
+        storage.setItem("x", "this")
+        storage.removeItem("x")
+        x = storage.getItem("x")
+        assert x == None
