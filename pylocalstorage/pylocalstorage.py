8438a1b258a4eca0c3802158ac9be9170f88632c
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2022-09-16 23:20:13 -0400

    Update pylocalstorage.py

--- pylocalstorage/pylocalstorage.py
+++ pylocalstorage/pylocalstorage.py
@@ -1,66 +1,73 @@
-from os.path import split, abspath, isfile, exists
-from os import remove, mkdir
-from json import load, dumps
-from glob import glob
+from os.path import split, abspath
+from sqlalchemy import create_engine
+
+engine = None
 
 
-class LocalStorage:
+def init():
+    # Getting PATH of file
+    pathname, _ = split(abspath(__file__))
+    global engine
+    engine = create_engine(f"sqlite:///{pathname}/localStorage.db", echo=False)
+    engine.execute("""
+    CREATE TABLE IF NOT EXISTS LocalStorage (
+        key TEXT,
+        value TEXT
+    );
+    """)
 
-    __version__ = "1.1.1"
-    __filename = None
-    length = 0
-
-    def __init__(self):
-        # Getting PATH of file
-        pathname, _ = split(abspath(__file__))
-        pathname += "/data"
-        if not exists(pathname):
-            mkdir(pathname)
-        self.__filename = pathname + "/{}.json"
-        self.__list_json = lambda: glob(pathname + "/*.json")
-        self.__update_length = lambda: len(self.__list_json())
-        self.length = self.__update_length()
-
-    def setItem(self, key, value):
-        try:
-            value_str = dumps(value)
-            with open(self.__filename.format(key), "w") as file:
-                print(value_str, file=file)
-            self.length = self.__update_length()
-        except:
-            raise WriteStorageError
-
-    def getItem(self, key):
-        fname = self.__filename.format(key)
-        if isfile(fname):
-            with open(fname) as file:
-                return load(file)
-
-    def removeItem(self, key):
-        fname = self.__filename.format(key)
-        if isfile(fname):
-            remove(fname)
-        self.length = self.__update_length()
-
-    def clear(self):
-        for fname in self.__list_json():
-            remove(fname)
-        self.length = self.__update_length()
-
-    def key(self, index):
-        if isinstance(index, int) and 0 <= index < self.length:
-            _, key = split(self.__list_json()[index])
-            return key.replace(".json", "")
+def getItem():
+    result = list(engine.execute("""
+    SELECT value
+    FROM LocalStorage
+    WHERE key = 'name';
+    """))
+    if result:
+        return result[0][0]
 
 
-class BaseError(Exception):
-    """Base class for other exceptions"""
-    message: str = None
+def setItem():
+    if getItem() is None:
+        engine.execute("""
+        INSERT INTO LocalStorage (key, value)
+        VALUES ('name','SendCommand');
+        """)
+    else:
+    	engine.execute("""
+        UPDATE LocalStorage
+        SET value = 'HDMIMonitorTest'
+        WHERE key = 'name';
+        """)
 
-    def __init__(self) -> None:
-        class_name = self.__class__.__name__
-        super().__init__(f"{class_name}(message={self.message})")
+
+def removeItem():
+    engine.execute("""
+    DELETE FROM LocalStorage
+    WHERE key = 'name';
+    """)
 
 
-class WriteStorageError(BaseError):
-    message = "Could not serialize the data"
+def key(index):
+    result = list(engine.execute("""
+    SELECT key
+    FROM LocalStorage;
+    """))
+    if result:
+        return result[index][0]
+
+
+def clear():
+    engine.execute("""
+    DELETE FROM LocalStorage;
+    """)
+
+
+init()
+setItem()
+print(getItem())
+setItem()
+print(getItem())
+print(key(0))
+removeItem()
+clear()
+print(getItem())
