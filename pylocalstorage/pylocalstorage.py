86d8f6a39dabe916f5de25b5688c3cd081cd0e33
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2022-09-17 15:29:16 -0400

    Update pylocalstorage.py

--- pylocalstorage/pylocalstorage.py
+++ pylocalstorage/pylocalstorage.py
@@ -1,56 +1,73 @@
-from os.path import split, abspath, isfile, exists
-from os import remove, mkdir
-from json import load, dumps
-from glob import glob
+from os.path import split, abspath
+from sqlalchemy import create_engine
+from json import dumps, loads
 
 
 class LocalStorage:
 
-    __version__ = "1.1.1"
-    __filename = None
+    __version__ = "1.2.0"
+    __engine = None
     length = 0
 
-    def __init__(self):
+    def __init__(self) -> None:
         # Getting PATH of file
         pathname, _ = split(abspath(__file__))
-        pathname += "/data"
-        if not exists(pathname):
-            mkdir(pathname)
-        self.__filename = pathname + "/{}.json"
-        self.__list_json = lambda: glob(pathname + "/*.json")
-        self.__update_length = lambda: len(self.__list_json())
-        self.length = self.__update_length()
+        self.__engine = create_engine(f"sqlite:///{pathname}/localStorage.db")
+        self.__engine.execute("""
+        CREATE TABLE IF NOT EXISTS LocalStorage (
+            key TEXT UNIQUE,
+            value TEXT
+        );
+        """)
+        self.length = self.__get_length()
 
-    def setItem(self, key, value):
+    def setItem(self, key, value) -> None:
         try:
             value_str = dumps(value)
-            with open(self.__filename.format(key), "w") as file:
-                print(value_str, file=file)
-            self.length = self.__update_length()
+            self.__engine.execute(f"""
+            INSERT INTO LocalStorage (key, value)
+            VALUES ('{key}','{value_str}')
+            ON CONFLICT(key) DO UPDATE SET value = '{value_str}';
+            """)
+            self.length = self.__get_length()
         except:
             raise WriteStorageError
 
     def getItem(self, key):
-        fname = self.__filename.format(key)
-        if isfile(fname):
-            with open(fname) as file:
-                return load(file)
+        result = list(self.__engine.execute(f"""
+        SELECT value
+        FROM LocalStorage
+        WHERE key = '{key}';
+        """))
+        if result:
+            return loads(result[0][0])
 
-    def removeItem(self, key):
-        fname = self.__filename.format(key)
-        if isfile(fname):
-            remove(fname)
-        self.length = self.__update_length()
+    def removeItem(self, key) -> None:
+        self.__engine.execute(f"""
+        DELETE FROM LocalStorage
+        WHERE key = '{key}';
+        """)
+        self.length = self.__get_length()
 
-    def clear(self):
-        for fname in self.__list_json():
-            remove(fname)
-        self.length = self.__update_length()
+    def clear(self) -> None:
+        self.__engine.execute("""
+        DELETE FROM LocalStorage;
+        """)
+        self.length = self.__get_length()
 
-    def key(self, index):
+    def key(self, index: int):
         if isinstance(index, int) and 0 <= index < self.length:
-            _, key = split(self.__list_json()[index])
-            return key.replace(".json", "")
+            result = list(self.__engine.execute("""
+            SELECT key
+            FROM LocalStorage;
+            """))
+            return result[index][0]
+
+    def __get_length(self) -> int:
+        result = list(self.__engine.execute("""
+        SELECT COUNT(*) FROM LocalStorage;
+        """))
+        return result[0][0]
 
 
 class BaseError(Exception):
