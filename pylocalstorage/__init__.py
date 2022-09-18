bf3b32879f7ff1951374d2043ef17460322874a7
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2022-09-18 00:58:59 -0400

    Update __init__.py

--- pylocalstorage/__init__.py
+++ pylocalstorage/__init__.py
@@ -1,1 +1,9 @@
 from .pylocalstorage import LocalStorage, WriteStorageError
+from os import system
+
+try:
+    from sqlalchemy import create_engine
+except:
+    system("pip install SQLAlchemy")
+    from sqlalchemy import create_engine
+
