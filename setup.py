8501401a049ed772b702cbc39ad358fc5beb1ec5
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2023-10-04 02:44:38 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
 
-with open('README.md', 'r') as readmefile:
+with open("README.md") as readmefile:
     readme = readmefile.read()
 
 setup(
-    name='pylocalstorage',
-    version='1.2.1',
-    author='David Ferreira',
-    author_email='ferreirad08@gmail.com',
-    description='A package to store data on the hard disk (HD) and make it available to all Python applications running locally!',
+    name="pylocalstorage",
+    version="1.3.0",
+    author="David Ferreira",
+    author_email="ferreirad08@gmail.com",
+    description="A package to store data on the hard disk (HD) and make it available to all Python applications running locally!",
     long_description=readme,
-    long_description_content_type='text/markdown',
-    url='https://github.com/ferreirad08/pylocalstorage',
+    long_description_content_type="text/markdown",
+    url="https://github.com/ferreirad08/pylocalstorage",
     packages=find_packages(),
     install_requires=[],
-    license='MIT',
+    license="MIT",
     classifiers=[
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
@@ -26,7 +26,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=2.7',
+    python_requires=">=2.7",
     project_urls={
         "Source": "https://github.com/ferreirad08/pylocalstorage",
     }
