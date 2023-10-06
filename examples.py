818cb556ebca371c3bf1be0687a06423c00fa53d
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2023-10-06 16:32:46 -0400

    Update examples.py

--- examples.py
+++ examples.py
@@ -1,4 +1,6 @@
-from pylocalstorage import LocalStorage
+from pylocalstorage import LocalStorage, ObjectPersistence
+
+print("# Tests with LocalStorage instance\n")
 
 # Connecting to LocalStorage
 ls = LocalStorage()
@@ -28,3 +30,23 @@ for i in range(ls.length):
 # Cleaning up LocalStorage
 ls.clear()
 print("Number of items:", ls.length)
+
+
+
+
+print("\n# Tests with ObjectPersistence instance\n")
+
+# Connecting to ObjectPersistence
+op = ObjectPersistence()
+
+op.setItem("test", "Hello")
+op.clear()
+
+op.setItem("test", {
+    "country": "Brazil",
+    "city": "Manaus"
+})
+print(op.getItem("test"))
+
+op.removeItem("test")
+print(op.getItem("test"))
