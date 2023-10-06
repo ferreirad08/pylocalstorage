b27c43f49c82516dc1311a6ceb67c2501465af9f
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2023-10-06 15:14:20 -0400

    Update __init__.py

--- pylocalstorage/__init__.py
+++ pylocalstorage/__init__.py
@@ -4,4 +4,5 @@ except:
     from os import system
     system("pip install SQLAlchemy")
 
-from .localstorage import LocalStorage, WriteStorageError
+from .local_storage import LocalStorage
+from .exceptions import WriteStorageError
