b36f1938b7580144ae427352a01f35323b48246e
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2024-01-02 16:58:59 -0400

    Update pylocalstorage.py

--- pylocalstorage/pylocalstorage.py
+++ pylocalstorage/pylocalstorage.py
@@ -1,73 +1,47 @@
-from threading import Lock
-from json import dumps, loads
-
-from sqlalchemy import create_engine, text
+import os
+import pickle
 
 
 class LocalStorage:
+    __version__ = "1.4.0"
 
-    __version__ = "1.3.0"
-
-    __lock = Lock()
-    __dbpath = __file__.replace("pylocalstorage.py", "localStorage.db")
-    __engine = create_engine(f"sqlite:///{__dbpath}")
-
-    length = 0
+    path = __file__.replace("main.py", "buffer/")
 
     def __init__(self) -> None:
-        self.__executeQuery("""
-        CREATE TABLE IF NOT EXISTS LocalStorage (
-            key TEXT UNIQUE,
-            value TEXT
-        );
-        """)
-        self.__update_length()
+        if not os.path.exists(self.path):
+            os.mkdir(self.path)
 
     def setItem(self, key, value) -> None:
         try:
-            value_str = dumps(value)
-            self.__executeQuery(f"""
-            INSERT INTO LocalStorage (key, value)
-            VALUES ('{key}','{value_str}')
-            ON CONFLICT(key) DO UPDATE SET value = '{value_str}';
-            """)
-            self.__update_length()
+            with open(f"{self.path}{key}.p", "wb") as pfile:
+                pickle.dump(value, pfile)
         except:
             raise WriteStorageError
 
     def getItem(self, key):
-        result = list(self.__executeQuery(f"""
-        SELECT value
-        FROM LocalStorage
-        WHERE key = '{key}';
-        """))
-        if result:
-            return loads(result[0][0])
+        fname = f"{self.path}{key}.p"
+
+        if os.path.isfile(fname):
+            with open(fname, "rb") as pfile:
+                return pickle.load(pfile)
 
     def removeItem(self, key) -> None:
-        self.__executeQuery(f"""
-        DELETE FROM LocalStorage
-        WHERE key = '{key}';
-        """)
-        self.__update_length()
+        fname = f"{self.path}{key}.p"
+
+        if os.path.isfile(fname):
+            os.remove(fname)
 
     def clear(self) -> None:
-        self.__executeQuery("DELETE FROM LocalStorage;")
-        self.__update_length()
+        for name in os.listdir(self.path):
+            os.remove(f"{self.path}{name}")
 
-    def key(self, index: int):
+    def key(self, index: int) -> str:
         if isinstance(index, int) and 0 <= index < self.length:
-            result = self.__executeQuery("SELECT key FROM LocalStorage;")
-            return list(result)[index][0]
+            return os.listdir(self.path)[index][:-2]
 
-    def __update_length(self) -> None:
-        result = self.__executeQuery("SELECT COUNT(*) FROM LocalStorage;")
-        self.length = list(result)[0][0]
-
-    def __executeQuery(self, query):
-        with self.__lock:
-            with self.__engine.begin() as connection:
-                return connection.execute(text(query))
+    @property
+    def length(self):
+        return len(os.listdir(self.path))
 
 
 class BaseError(Exception):
